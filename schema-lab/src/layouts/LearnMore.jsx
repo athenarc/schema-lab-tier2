2f8697808144f5645114674268271d9bcf24f210
Panagiotis Deligiannis <deligianp@athenarc.gr>
2026-03-24 22:59:48 +0200

    Resolve #2: Update "Learn more" view

--- schema-lab/src/layouts/LearnMore.jsx
+++ schema-lab/src/layouts/LearnMore.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
 import { Container, Row, Col, ListGroup } from 'react-bootstrap';
+import {Link} from "react-router-dom";
+import Image from "react-bootstrap/Image";
+import eu from "../img/about_us/EN_FundedbytheEU_RGB_POS.png";
+import tier2 from "../img/about_us/63a57763ef1dd939001828.png";
 
 
 const LearnMore = () => {
@@ -41,6 +45,27 @@ const LearnMore = () => {
           </ListGroup>
         </Col>
       </Row>
+    <Row className="mt-4">
+        <Col>
+            <ListGroup variant="flush">
+                <ListGroup.Item className="border-0 text-muted">
+                    <strong>Computational Experiments</strong>
+                </ListGroup.Item>
+                <ListGroup.Item className="border-0 text-muted">
+                    This interface provides a streamlined experience for creating and managing computational experiments.
+                </ListGroup.Item>
+                <ListGroup.Item className="border-0 text-muted">
+                    <strong>Create Experiments:</strong> Combine one or more tasks into a computational experiment.
+                </ListGroup.Item>
+                <ListGroup.Item className="border-0 text-muted">
+                    <strong>Manage Tasks:</strong> Easily select and group tasks using checkboxes.
+                </ListGroup.Item>
+                <ListGroup.Item className="border-0 text-muted">
+                    <strong>View Details:</strong> Access detailed information about each task and experiment.
+                </ListGroup.Item>
+            </ListGroup>
+        </Col>
+    </Row>
       <Row className="mt-3">
         <Col>
           <p>
@@ -48,7 +73,27 @@ const LearnMore = () => {
           </p>
         </Col>
       </Row>
-
+        <Row className="justify-content-center">
+            <Col className="justify-content-center mb-4">
+                <Row>
+                    <Col xs={12}>
+                        SCHEMA lab was developed during <Link to={'https://www.tier2-project.eu/'} target={'_blank'}>TIER2 project</Link>. It receives funding from the European Union's Horizon Europe research and innovation programme under grant agreement No 101094817. Views and opinions expressed are those of the author(s) only and do not necessarily reflect those of the European Union or the European Research Executive Agency (REA). Neither the EU nor REA can be held responsible for them.
+                    </Col>
+                </Row>
+                <Row className="d-flex justify-content-center mt-4">
+                    <Image
+                        src={eu}
+                        alt='EU flag'
+                        style={{ width: 'auto', height: '50px' }}
+                    />
+                    <Image
+                        src={tier2}
+                        alt='EU flag'
+                        style={{ width: 'auto', height: '50px' }}
+                    />
+                </Row>
+            </Col>
+        </Row>
     </Container>
   );
 };
